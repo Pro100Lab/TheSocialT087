01916e70cf26995b8cbfe00e6c44149fd5d96851
Max <RazeTheShadow@yandex.ru>
2024-11-21 00:50:02 +0500

    

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,24 +1,23 @@
 
 export const is_debug = process.env.NODE_ENV === 'development';
-export const debug_endpoint = 'spacecraft-it.ru/api';
+export const debug_endpoint = 'spacecraft-it.ru';
 export const schema = 'https';
 export const debug_schema = 'https';
 export const endpoint_version = 'v1';
 export const wss_schema = 'wss';
 export const ws_schema = 'ws';
 
-const endpoint = debug_endpoint;
 
 
 export function getURL(urn) {
     if(urn[0] === '/') {
         urn = urn.substring(1);
     }
-    return `${is_debug ? debug_schema : schema}://${is_debug ? debug_endpoint : endpoint}/${urn}`
+    return `${schema}://${debug_endpoint}/api/${urn}`
 }
 
 export function getWSSUrl(urn) {
-    return `${is_debug ? ws_schema : wss_schema }://${is_debug ? debug_endpoint : endpoint}/${urn}`
+    return `${wss_schema}://${is_debug}/api/wss/${urn}`
 }
 
 export function getStatic(src) {
