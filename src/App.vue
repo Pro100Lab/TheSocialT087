b2f322880e5f05ddef6d2bb9803dc78172df741e
Max <RazeTheShadow@yandex.ru>
2024-11-22 02:29:04 +0500

    improved mobiles

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,34 @@
   <v-app>
 
     <v-main class="main-display main-display__bg" :class="isMobile ? 'pa-2' : 'pa-4'" v-if="!showPresentation">
-        <v-row dense :class="isMobile ? '' : ''" >
+      <v-row dense :class="isMobile ? '' : ''" >
 
-          <v-col cols="12">
-            <router-view :show-sms="showSMS" class="main-display__main-view"/>
-          </v-col>
+        <v-col cols="12">
+          <router-view :show-sms="showSMS" class="main-display__main-view"/>
+        </v-col>
 
-          <v-col cols="12">
-            <display-switch class="main-display__secondary-view"></display-switch>
-          </v-col>
-        </v-row>
+        <v-col v-if="!isMobile" cols="12">
+          <display-switch :is-mobile="isMobile" :switch-display="onDisplaySwitch" class="main-display__secondary-view"></display-switch>
+        </v-col>
+        <v-col v-else>
+          <v-btn color="grey-darken-4" block append-icon="mdi-chevron-down" text="Экраны" v-on:click="showDisplays = true"></v-btn>
+        </v-col>
+      </v-row>
     </v-main>
+
+    <v-card v-if="isMobile && showDisplays" style="position: fixed; bottom: 0; width: 100%;" color="grey-darken-4">
+      <v-card-actions>
+        <v-card-title>Экраны</v-card-title>
+        <v-spacer/>
+        <v-btn variant="icon"
+               icon="mdi-close"
+               v-on:click="showDisplays = false"></v-btn>
+      </v-card-actions>
+      <display-switch
+              :is-mobile="isMobile"
+              :switch-display="onDisplaySwitch"
+      ></display-switch>
+    </v-card>
 
     <transition name="fade" mode="in-out">
       <show-presents v-if="showPresentation" class="main-display presents-sticky" :on-close="presentationsShowed"></show-presents>
@@ -37,94 +54,98 @@
 
 <script>
 
-import DisplaySwitch from "@/components/DisplaySwitch";
-// import LogDisplay from "@/components/LogDisplay";
-import ShowPresents from "@/views/ShowPresents";
+  import DisplaySwitch from "@/components/DisplaySwitch";
+  // import LogDisplay from "@/components/LogDisplay";
+  import ShowPresents from "@/views/ShowPresents";
 
-export default {
-  name: 'App',
-  components: {
-    ShowPresents,
-    DisplaySwitch,
-    // LogDisplay
-  },
-  data: () => ({
-    showPresentation: true,
-    smsShow: false,
-    smsAnimation: '',
-    sms: {
-      number: null,
-      text: null,
-      date: null,
+  export default {
+    name: 'App',
+    components: {
+      ShowPresents,
+      DisplaySwitch,
+      // LogDisplay
     },
-    smsList: []
-  }),
-  mounted() {
-    setInterval(() => {this.showRandomSMS()}, 30000)
-  },
-  methods: {
-    showRandomSMS() {
-      const randList = ['e-bank', 'style-bank', 'b-bank', 'c-bank', 'k-bank', 'pop-socket-bank', 'tururu',
-      'financial-pyramides', 'SPORT LOTO', 'vacation-bank', 'finchill', 'domdon-bank'];
-      const shouldUseRandList = Math.random() * 100 > 50;
-      let number = null;
-      if( shouldUseRandList )
-        number = this.getRandText(randList);
-      else {
-        number = `8${Math.ceil(Math.random()*(10**10))}`;
-      }
-
-      const texts = ['Объединяйтесь с близкими и получайте кешбэк 5% рублями на супермаркеты',
-        'Кешбек 100% на доставку продуктов в Е-пятницу! Крутите барабан до 22 декабря и испытайтся свою удачу',
-        'Разбираемся, как компании меняют число акций и как это влияет...',
-      'Дарим кешбэк за покупку в супермаркете, кафе, в магазине одежды и обуви',
-              'Платите за коммунальные услуги онлайн через наш банк с 23.11 по 10.12 и получите шанс выиграть...'
-      ];
-
-      const shouldUseRandText = Math.random() * 100 > 70;
-      let text = null;
-      if( shouldUseRandText )
-        text = this.getRandText(texts);
-      else {
-        const services = ['кэшбэк', 'кредит', 'заём', 'инвестиции'];
-        let is = ['отличное предложение', 'полный улёт', 'только у нас']
-        for( let i = 0; i <  100; i+=10 )
-          is.push(`${i}%`)
-
-        let very = ['очень', 'супер', 'супер-пупер', 'мега', 'ультра', 'невиданно'];
-        let deal = ['выгодное', 'красивое', ' только у нас', 'потрясающее'];
-
-        text = `${this.getRandText(is)}: ${this.getRandText(services)} - ${this.getRandText(very)} ${this.getRandText(deal)}. Приходите к нам скорей!`
-      }
-      this.showSMS(number, text);
+    data: () => ({
+      showPresentation: true,
+      smsShow: false,
+      smsAnimation: '',
+      sms: {
+        number: null,
+        text: null,
+        date: null,
+      },
+      smsList: [],
+      showDisplays: false,
+    }),
+    mounted() {
+      setInterval(() => {this.showRandomSMS()}, 30000)
     },
-    getRandText(container) {
-     return container[Math.ceil(Math.random() * container.length)] || container[0];
-    },
-    showSMS(number, text) {
-      this.smsShow = true;
-      this.sms = {number, text, date: new Date()}
-      this.smsList.push(this.sms);
-      this.smsAnimation = 'scrollable'
-      setTimeout(() => {
-        this.smsAnimation = 'scrollable-reverse'
+    methods: {
+      onDisplaySwitch() {
+          this.showDisplays = false;
+      },
+      showRandomSMS() {
+        const randList = ['e-bank', 'style-bank', 'b-bank', 'c-bank', 'k-bank', 'pop-socket-bank', 'tururu',
+          'financial-pyramides', 'SPORT LOTO', 'vacation-bank', 'finchill', 'domdon-bank'];
+        const shouldUseRandList = Math.random() * 100 > 50;
+        let number = null;
+        if( shouldUseRandList )
+          number = this.getRandText(randList);
+        else {
+          number = `8${Math.ceil(Math.random()*(10**10))}`;
+        }
+
+        const texts = ['Объединяйтесь с близкими и получайте кешбэк 5% рублями на супермаркеты',
+          'Кешбек 100% на доставку продуктов в Е-пятницу! Крутите барабан до 22 декабря и испытайтся свою удачу',
+          'Разбираемся, как компании меняют число акций и как это влияет...',
+          'Дарим кешбэк за покупку в супермаркете, кафе, в магазине одежды и обуви',
+          'Платите за коммунальные услуги онлайн через наш банк с 23.11 по 10.12 и получите шанс выиграть...'
+        ];
+
+        const shouldUseRandText = Math.random() * 100 > 70;
+        let text = null;
+        if( shouldUseRandText )
+          text = this.getRandText(texts);
+        else {
+          const services = ['кэшбэк', 'кредит', 'заём', 'инвестиции'];
+          let is = ['отличное предложение', 'полный улёт', 'только у нас']
+          for( let i = 0; i <  100; i+=10 )
+            is.push(`${i}%`)
+
+          let very = ['очень', 'супер', 'супер-пупер', 'мега', 'ультра', 'невиданно'];
+          let deal = ['выгодное', 'красивое', ' только у нас', 'потрясающее'];
+
+          text = `${this.getRandText(is)}: ${this.getRandText(services)} - ${this.getRandText(very)} ${this.getRandText(deal)}. Приходите к нам скорей!`
+        }
+        this.showSMS(number, text);
+      },
+      getRandText(container) {
+        return container[Math.ceil(Math.random() * container.length)] || container[0];
+      },
+      showSMS(number, text) {
+        this.smsShow = true;
+        this.sms = {number, text, date: new Date()}
+        this.smsList.push(this.sms);
+        this.smsAnimation = 'scrollable'
         setTimeout(() => {
-          this.sms = {number: null, text: null}
-          this.smsShow = false;
-        }, 1500)
-      }, 5000)
-    },
+          this.smsAnimation = 'scrollable-reverse'
+          setTimeout(() => {
+            this.sms = {number: null, text: null}
+            this.smsShow = false;
+          }, 1500)
+        }, 5000)
+      },
 
-    presentationsShowed: function () {
-      this.showPresentation = false;
-    }
-  },
-  computed: {
-    isMobile() {
-      return window.innerWidth < 980;
+      presentationsShowed: function () {
+        this.showPresentation = false;
+      }
+    },
+    computed: {
+      isMobile() {
+        return window.innerWidth < 980;
+      }
     }
   }
-}
 </script>
 
 <style scoped>
@@ -137,6 +158,13 @@ export default {
     max-height: 80vh;
   }
 
+  @media screen and (max-width: 960px) {
+    .main-display__main-view {
+      min-height: 90vh;
+      max-height: 90vh;
+    }
+
+  }
   .main-display {
     width: 100vw;
     height: 100vh;
--- src/components/DisplaySwitch.vue
+++ src/components/DisplaySwitch.vue
@@ -1,13 +1,14 @@
 <template>
-    <v-sheet color="transparent">
-        <div class="d-flex flex-row justify-center">
+    <v-sheet color="transparent" :class="isMobile? 'mobile-scroll' : ''">
+        <div :class="isMobile ? 'd-flex flex-column align-center' : 'd-flex flex-row justify-center'">
             <template  v-for="display of displayList"
                        :key="`display-mini-${display.id}`">
                 <v-hover v-slot="{ isHovering, props }">
                     <v-card
+                            variant="flat"
                             color="transparent"
-                            class="mx-2"
-                            v-on:click="$router.push(display.component)"
+                            :class="isMobile? 'my-2' : 'mx-2'"
+                            v-on:click="handleRoute(display.component)"
                     >
                         <v-img
                                 :class="isHovering ? 'zoom' : 'no-zoom'"
@@ -37,17 +38,28 @@
     export default {
         name: "DisplaySwitch",
         props: {
-            switchDisplay: Function
+            switchDisplay: Function,
+            isMobile: Boolean
         },
         data: () => {
             return {
                 displayList
+            }
+        },
+        methods: {
+            handleRoute(name) {
+                this.switchDisplay(name);
+                this.$router.push(name);
             }
         }
     }
 </script>
 
 <style>
+    .mobile-scroll {
+        max-height: 50%;
+        overflow-y: auto
+    }
     .v-responsive {.v-img__img { transition: all 0.2s; } &.zoom { .v-img__img { transform: scale(1.2); } } }
     .v-responsive {.v-img__img { transition: all 0.2s; } &.no-zoom { .v-img__img { transform: scale(1); } } }
 </style>
